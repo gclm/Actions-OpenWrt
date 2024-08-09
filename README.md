14ee61acb91763a041ca508e5a4cb868cc1409a0
gclm <gclmit@163.com>
2024-08-09 20:30:00 +0800

    feat: 初始化仓库
    fix: 修复github action报错
    
    fix: 调整固件包
    
    fix: 调整固件包
    
    fix: 调整固件包
    
    fix: 减少编译固件
    
    fix: 优化编译环境
    
    fix: 调整github action编译
    
    fix: 优化编译代码
    
    fix: 调整openwrt配置文件
    
    fix: 调整github action编译参数
    
    fix: 调整目录结构
    
    build: 调整github action
    
    fix: 添加编译组件
    
    fix: 添加openclash插件
    
    fix: 更新redmi文档
    
    fix: 调整编译文件
    
    fix: 调整编译依赖文件
    
    fix: 调整编译依赖文件
    
    fix: 调整编译依赖文件
    
    fix: 调整编译文件
    
    fix: 调整编译环境参数
    
    fix: 修复变异打包错误
    
    fix: 优化action
    
    fix: 测试发布权限

---
+++ README.md
@@ -0,0 +1,32 @@
+# OpenWrt 在线编译
+
+### 固件发布:
+
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/gclm/Actions-OpenWrt?style=for-the-badge&label=固件下载)](https://github.com/gclm/Actions-OpenWrt/releases/latest)
+
+#### 固件源码来源
+
+- hanwckf-[hanwckf/immortalwrt-mt798x](https://github.com/hanwckf/immortalwrt-mt798x).
+
+```bash
+git clone -b openwrt-21.02 --depth=1 https://github.com/hanwckf/immortalwrt-mt798x
+```
+
+- istore-[linkease/istore](https://github.com/linkease/istore.git).
+
+```bash
+git clone --depth=1 https://github.com/linkease/istore.git
+```
+
+- k9_package-[kiddin9/openwrt-packages](https://github.com/kiddin9/openwrt-packages.git).
+
+```bash
+git clone --depth=1 https://github.com/kiddin9/openwrt-packages.git
+```
+
+### 固件初始配置
+
+- 默认管理IP:`192.168.33.1`
+- 用户名:`root`
+- 密码:`设置首次登录后台密码为空（进入openwrt后自行修改密码）`
+
---
+++ redmi-ax6000/diy-part2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "设置lan ip"
+lan_ip='192.168.33.1'
+sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
+
+echo "修改主机名"
+device_name='Redmi'
+sed -i "s/ImmortalWrt/$device_name/g" ./package/base-files/files/bin/config_generate
+
+# 修改 界面主题
+echo "修改界面主题"
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+echo "修改时区"
+utc_name='Asia\/Shanghai'
+sed -i "s/'UTC'/'CST-8'\n                set system.@system[-1].zonename='$utc_name/g" package/base-files/files/bin/config_generate
+
+echo "修改wifi名称"
+wifi_name=gclm
+sed -i "s/ImmortalWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i "s/ImmortalWrt-2.4G/$wifi_name-2.4G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/$wifi_name-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/MT7986_AX6000_2.4G/$wifi_name-2.4G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
+sed -i "s/MT7986_AX6000_5G/$wifi_name-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
