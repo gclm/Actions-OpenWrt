c289ef74297c26f19caefaa1f38dfcf2f6b312f8
gclm <gclmit@163.com>
2024-08-13 09:40:01 +0800

    feat: 新增ntp服务

--- redmi-ax6000/diy-part2.sh
+++ redmi-ax6000/diy-part2.sh
@@ -8,18 +8,18 @@ echo "修改主机名"
 device_name='Redmi'
 sed -i "s/ImmortalWrt/$device_name/g" ./package/base-files/files/bin/config_generate
 
-# 修改 界面主题
-echo "修改界面主题"
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
 echo "修改时区"
 utc_name='Asia\/Shanghai'
 sed -i "s/'UTC'/'CST-8'\n                set system.@system[-1].zonename='$utc_name/g" package/base-files/files/bin/config_generate
+
+echo "启用ntp"
+sed -i "s/system\.ntp\.enabled=\'1\'/system\.ntp\.enabled=\'0\'/g" package/base-files/files/bin/config_generate
+
+echo "修改界面主题"
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 echo "修改wifi名称"
 wifi_name=gclm
 sed -i "s/ImmortalWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i "s/ImmortalWrt-2.4G/$wifi_name/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 sed -i "s/ImmortalWrt-5G/$wifi_name-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
-sed -i "s/MT7986_AX6000_2.4G/$wifi_name/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b0.dat
-sed -i "s/MT7986_AX6000_5G/$wifi_name-5G/g" package/mtk/drivers/wifi-profile/files/mt7981/mt7981.dbdc.b1.dat
